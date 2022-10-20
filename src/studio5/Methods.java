63b1f0a1cbb0ae650e162722ebb96207a59ac7ce
haile <haile@DESKTOP-PMUC6VG.dhcp.wustl.edu>
2022-10-20 14:05:19 -0500

    Studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,6 +16,9 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
+		double xDistance = Math.pow((x1 - x2), 2);
+		double yDistance = Math.pow((y1 -y2), 2);
+		distance = Math.sqrt
 		
 		return distance;
 	}
@@ -62,7 +65,17 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
-		
+		for (int i = 0; i < source.length(); i++)
+		{
+			if (source.charAt(i) == target)
+			{
+				result = result + replacement;
+			}
+			else
+			{
+				result = result + source.charAt(i);
+			}
+		}
 		return result;
 	}
 
@@ -75,6 +88,10 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
+		for (int i = 0; i < values.length; i++)
+		{
+			sum += values[i];
+		}
 		
 		return sum;
 	}
@@ -87,9 +104,12 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
-
+		for (int i = 0; i < values.length; i++)
+		{
+			values[i] += value;
+		}
 		
 
 		return values;
@@ -97,6 +117,15 @@ public class Methods {
 
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
+	/**
+	 * 
+	 * @param values an array of integers
+	 * @return mean of the integers in the array
+	 */
+	public static double arrayMean(int[] values)
+	{
+		return (double)arraySum(values) / values.length;
+	}
 
 	
 }
